490c965c3495807d6474fba75696b582bdeb3bcf
LeBrontao <1332247492@qq.com>
2018-12-18 14:22:07 +0800

    代码

---
+++ ReturnToSpace/Assets/_JiaoBen/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        if(agent.remainingDistance==0)
+        {
+            animator.Play("HumanoidIdle");
+        }
+        else
+        {
+            animator.Play("HumanoidRun");
+        }
+
+
+    }
+}
