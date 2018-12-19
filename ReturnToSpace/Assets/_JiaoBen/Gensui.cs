39a370d688b015bddf2f45b89ede53c32eb81b33
Xia-Jialin <18253040259@163.com>
2018-12-19 11:05:34 +0800

    实现摄像机跟随

---
+++ ReturnToSpace/Assets/_JiaoBen/Gensui.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gensui : MonoBehaviour {
+    private Vector3 offset;
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+        offset = player.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, player.position - offset, Time.deltaTime*10);
+	}
+}
--- ReturnToSpace/Assets/_JiaoBen/Player.cs
+++ ReturnToSpace/Assets/_JiaoBen/Player.cs
@@ -21,24 +21,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 vector3;
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit))
             {
-                agent.SetDestination(hit.point);
+                vector3 = new Vector3(hit.point.x,this. transform.position.y, hit.point.z);
+                Debug.Log(hit.point);
+                agent.SetDestination(vector3);
+                Debug.Log(vector3);
+               
+                
             }
+           
         }
-        if(agent.remainingDistance==0)
+        Debug.Log(agent.remainingDistance);
+        if (agent.remainingDistance==0)
         {
+            Debug.Log("123");
             animator.Play("HumanoidIdle");
         }
         else
-        {
+        { 
             animator.Play("HumanoidRun");
         }
-
-
     }
 }
